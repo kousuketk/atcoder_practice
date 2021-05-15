02fdff41434bdd88fb190375d1e9c098964e39aa
kousuke takahashi <pamarikomi_t@icloud.com>
2021-05-15 13:30:15 +0900

    create template

---
+++ lib/01template.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <algorithm>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using ll = long long;
+using Graph = vector<vector<int>>;
+
+int main() {
+}
