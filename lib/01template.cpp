8e9b6a500009c9d12b30448e85fba67b41b4e2ea
kousuke takahashi <pamarikomi_t@icloud.com>
2021-05-21 10:26:16 +0900

    modify template

--- lib/01template.cpp
+++ lib/01template.cpp
@@ -4,6 +4,7 @@ using namespace std;
 #define rep(i,n) for (int i = 0; i < (n); ++i)
 using ll = long long;
 using P = pair<int,int>;
+using T = tuple<int,int, int>;
 using Graph = vector<vector<int>>;
 
 int main() {
