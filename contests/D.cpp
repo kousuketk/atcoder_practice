188d3d3c55acd2414fd5243108f210457481a386
kousuke takahashi <pamarikomi_t@icloud.com>
2021-06-27 00:50:47 +0900

    create meController

--- contests/D.cpp
+++ contests/D.cpp
@@ -13,4 +13,24 @@ void printVec(vector<int> &vec) {
 }
 
 int main() {
+  int N; cin >> N;
+  vector<P> s(N);
+  vector<P> t(N);
+  rep(i,N) {
+    int a, b; cin >> a >> b;
+    s[i] = P(a, b);
+  }
+  rep(i,N) {
+    int a, b; cin >> a >> b;
+    t[i] = P(a, b);
+  }
+  for(int i = 0; i < N; i++) {
+    int a = s[i].first; 
+    int b = s[i].second;
+    for(int j = 0; j < N; j++) {
+      int c = 0, d = 0;
+      //t[j]をs[i]にする、移
+      // s[i], t[j]を(0,0)にして
+    }
+  }
 }
