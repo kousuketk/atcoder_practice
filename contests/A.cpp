9954df7dc43e459f20176f6051b1643645f6e667
kousuke takahashi <pamarikomi_t@icloud.com>
2021-07-06 22:21:22 +0900

    modify

--- contests/A.cpp
+++ contests/A.cpp
@@ -13,30 +13,4 @@ void printVec(vector<int> &vec) {
 }
 
 int main() {
-  string S; cin >> S;
-  reverse(S.begin(), S.end());
-  int N = S.size();
-  // 逆転させたSの先頭から見ていく
-  // esare, resare, maerd, remaerd
-  // idx == S.size()-1 になったらok
-  int idx = 0;
-  int count = 0;
-  while(count < 10) {
-    // 5の時
-    count++;
-    if(idx > (N-5)) {
-      cout << "NO" << endl;
-      return 0;
-    }
-    string tmp_s = S.substr(idx, 5);
-    cout << tmp_s << endl;
-    if(tmp_s == "esare" || tmp_s == "resare") {
-      idx += 5;
-      if(idx == N) {
-        cout << "YES" << endl;
-        return 0;
-      }
-      continue;
-    }
-  }
 }
