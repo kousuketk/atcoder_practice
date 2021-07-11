8616ddbac9f2d659362685257b62981ff1448a7d
kousuke takahashi <pamarikomi_t@icloud.com>
2021-07-11 21:37:39 +0900

    modify template

--- lib/01template.cpp
+++ lib/01template.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 #include <algorithm>
 #define rep(i,n) for (int i = 0; i < (n); ++i)
+#define ALL(a)  (a).begin(),(a).end()
 using ll = long long;
 using P = pair<int,int>;
 using T = tuple<int,int,int>;
