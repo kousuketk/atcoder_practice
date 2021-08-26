24bd3cf944b084eb990576fff6f83df42c6fafe1
kousuke takahashi <pamarikomi_t@icloud.com>
2021-08-26 23:13:41 +0900

    create template_go

---
+++ 202108/20th~31th/26_abc164_A.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	i, err := strconv.Atoi(sc.Text())
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	a, b := scanInt(), scanInt()
+
+	if b >= a {
+		fmt.Println("unsafe")
+	} else {
+		fmt.Println("safe")
+	}
+}
---
+++ lib/01template.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	i, err := strconv.Atoi(sc.Text())
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+}
