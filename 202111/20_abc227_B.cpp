deb137edcb5aa7a335dd73d4b72c6e84f750e42b
kousuke takahashi <pamarikomi_t@icloud.com>
2021-11-20 20:57:53 +0900

    20_abc227

---
+++ 202111/20_abc227_B.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <algorithm>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define ALL(a)  (a).begin(),(a).end()
+using ll = long long;
+using P = pair<int,int>;
+using T = tuple<int,int,int>;
+using Graph = vector<vector<int>>;
+const int INF = 1001001001;
+// const ll INF = 1100100100100100100;
+const ll MOD = 1000000007;
+void printVec(vector<int> &vec) {
+  for (auto itr = vec.begin(); itr != vec.end(); itr++) cout << *itr << " "; 
+  cout << endl;
+}
+vector<int> next_h = {1, -1, 0, 0};
+vector<int> next_w = {0, 0, 1, -1};
+
+int main() {
+  int N; cin >> N;
+  vector<int> vec(N);
+  rep(i,N) cin >> vec[i];
+  map<int, int> mp;
+  for(int i = 1; i <= 200; i++) {
+    for(int j = 1; j <= 200; j++) {
+      int tmp = 4*i*j + 3*i + 3*j;
+      mp[tmp] = 1;
+    }
+  }
+  int ans = 0;
+  rep(i,N) {
+    int s = vec[i];
+    if(!mp[s]) ans++;
+  }
+  cout << ans << endl;
+}
