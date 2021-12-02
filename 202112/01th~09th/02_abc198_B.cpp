b10a3e9fe6ad38cf5e58c312b41a57ace91d6757
kousuke takahashi <pamarikomi_t@icloud.com>
2021-12-02 11:15:18 +0900

    1202

---
+++ 202112/01th~09th/02_abc198_B.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <algorithm>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define ALL(a)  (a).begin(),(a).end()
+using ll = long long;
+using P = pair<int,int>;
+using T = tuple<int,int,int>;
+using Graph = vector<vector<int>>;
+const int INF = 1001001001;
+// const ll INF = 1100100100100100100;
+const ll MOD = 1000000007;
+void printVec(vector<int> &vec) {
+  for (auto itr = vec.begin(); itr != vec.end(); itr++) cout << *itr << " "; 
+  cout << endl;
+}
+vector<int> next_h = {1, -1, 0, 0};
+vector<int> next_w = {0, 0, 1, -1};
+
+int main() {
+  string s; cin >> s;
+  int N = s.size();
+  int idx = N;
+  while(idx > 0 && s[idx-1] == '0') {
+    idx--;
+  }
+  if(idx != N) s.erase(idx);
+  // 残りのsについて回分であるか？
+  if(s == string(s.rbegin(), s.rend())) cout << "Yes" << endl;
+  else cout << "No" << endl;
+}
---
+++ 202112/01th~09th/02_abc198_C.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <algorithm>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define ALL(a)  (a).begin(),(a).end()
+using ll = long long;
+using P = pair<int,int>;
+using T = tuple<int,int,int>;
+using Graph = vector<vector<int>>;
+const int INF = 1001001001;
+// const ll INF = 1100100100100100100;
+const ll MOD = 1000000007;
+void printVec(vector<int> &vec) {
+  for (auto itr = vec.begin(); itr != vec.end(); itr++) cout << *itr << " "; 
+  cout << endl;
+}
+vector<int> next_h = {1, -1, 0, 0};
+vector<int> next_w = {0, 0, 1, -1};
+
+int main() {
+  double R; cin >> R; 
+  ll X, Y; cin >> X >> Y;
+  double euclid = sqrt(X*X + Y*Y);
+  int ans = ceil(euclid / R);
+  if(ans == 1 && euclid != R) ans++;
+  cout << ans << endl;  
+}
