978649ff378d0eec9072585278fecc054e1ea737
kousuke takahashi <pamarikomi_t@icloud.com>
2022-02-16 21:26:59 +0900

    update lib

--- lib/unionFindTree_α.cpp
+++ lib/unionFindTree_α.cpp
@@ -2,49 +2,65 @@
 using namespace std;
 #include <algorithm>
 #define rep(i,n) for (int i = 0; i < (n); ++i)
+#define ALL(a)  (a).begin(),(a).end()
 using ll = long long;
 using P = pair<int,int>;
 using T = tuple<int,int,int>;
 using Graph = vector<vector<int>>;
 const int INF = 1001001001;
+// const ll INF = 1100100100100100100;
+const ll MOD = 1000000007;
 void printVec(vector<int> &vec) {
   for (auto itr = vec.begin(); itr != vec.end(); itr++) cout << *itr << " "; 
   cout << endl;
 }
-
+vector<int> next_h = {1, -1, 0, 0};
+vector<int> next_w = {0, 0, 1, -1};
 struct UnionFind {
-  vector<int> par; // par[i]:iの親の番号　(例) par[3] = 2 : 3の親が2
-  vector<int> size_vec;
-
-  UnionFind(int N) : par(N), size_vec(N) { //最初は全てが根であるとして初期化
-    for(int i = 0; i < N; i++) {
-      par[i] = i;
-      size_vec[i] = 1;
-    }
+  vector<int> par;
+  vector<int> siz;
+  UnionFind(int N) : par(N), siz(N) {
+    rep(i,N) par[i] = i, siz[i] = 1;
   }
-
-  int root(int x) { // データxが属する木の根を再帰で得る：root(x) = {xの木の根}
-    if (par[x] == x) return x;
+  int root(int x) {
+    if(par[x] == x) return x;
     return par[x] = root(par[x]);
   }
-
-  void unite(int x, int y) { // xとyの木を併合
-    int rx = root(x); //xの根をrx
-    int ry = root(y); //yの根をry
-    if (rx == ry) return; //xとyの根が同じ(=同じ木にある)時はそのまま
-    int tmp = size_vec[rx];
-    size_vec[rx] += size_vec[ry]; // rootを見ないとsize_vecは正しくない
-    size_vec[ry] += tmp;
-    par[rx] = ry; //xとyの根が同じでない(=同じ木にない)時：xの根rxをyの根ryにつける
+  void unite(int x, int y) {
+    int rx = root(x), ry = root(y);
+    if(rx == ry) return;
+    if(siz[rx] < siz[ry]) swap(rx, ry);
+    siz[rx] += siz[ry];
+    par[ry] = rx;
+    return;
   }
-
-  bool same(int x, int y) { // 2つのデータx, yが属する木が同じならtrueを返す
-    int rx = root(x);
-    int ry = root(y);
-    return rx == ry;
+  bool same(int x, int y) {
+    return root(x) == root(y);
   }
-
   int size(int x) {
-    return size_vec[root(x)]; // rootを見ないとsize_vecは正しくないためrootを設定
+    return siz[root(x)];
   }
 };
+
+int main() {
+  int N, M; cin >> N >> M;
+  vector<P> vec(M);
+  rep(i,M){
+    cin >> vec[i].first >> vec[i].second;
+    vec[i].first--, vec[i].second--;
+  }
+  int ans = 0;
+  rep(i,M) {
+    UnionFind uf(N);
+    rep(j,M) {
+      if(i == j) continue;  // i番目以外の橋をかける
+      int x = vec[j].first, y = vec[j].second;
+      uf.unite(x,y);
+    }
+    // 最後にi番目の橋をかけて、sameじゃなかったら+1していく
+    int x = vec[i].first, y = vec[i].second;
+    if(!uf.same(x,y)) ans++;
+  }
+  cout << ans << endl;
+  return 0;
+}
