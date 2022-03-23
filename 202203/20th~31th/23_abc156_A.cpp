71756fd365c0c5df258f80ad7cd1deacc7221738
kousuke takahashi <pamarikomi_t@icloud.com>
2022-03-23 23:37:50 +0900

    23_abc156

---
+++ 202203/20th~31th/23_abc156_A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <algorithm>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define ALL(a)  (a).begin(),(a).end()
+using ll = long long;
+using P = pair<int,int>;
+using T = tuple<int,int,int>;
+using Graph = vector<vector<int>>;
+const int INF = 1001001001;
+// const ll INF = 1100100100100100100;
+const ll MOD = 1000000007;
+void printVec(vector<int> &vec) {
+  for (auto itr = vec.begin(); itr != vec.end(); itr++) cout << *itr << " "; 
+  cout << endl;
+}
+vector<int> next_h = {1, -1, 0, 0};
+vector<int> next_w = {0, 0, 1, -1};
+
+int main() {
+  int N, R; cin >> N >> R;
+  int ans = R;
+  if(N < 10) {
+    ans = R + 100*(10-N);
+  }
+  cout << ans << endl;
+  return 0;
+}
---
+++ 202203/20th~31th/23_abc156_C.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <algorithm>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define ALL(a)  (a).begin(),(a).end()
+using ll = long long;
+using P = pair<int,int>;
+using T = tuple<int,int,int>;
+using Graph = vector<vector<int>>;
+const int INF = 1001001001;
+// const ll INF = 1100100100100100100;
+const ll MOD = 1000000007;
+void printVec(vector<int> &vec) {
+  for (auto itr = vec.begin(); itr != vec.end(); itr++) cout << *itr << " "; 
+  cout << endl;
+}
+vector<int> next_h = {1, -1, 0, 0};
+vector<int> next_w = {0, 0, 1, -1};
+
+int main() {
+  int N; cin >> N;
+  vector<int> vec(N);
+  int max_val = 1;
+  rep(i,N) {
+    int x; cin >> x;
+    max_val = max(max_val, x);
+    vec[i] = x;
+  }
+  int ans = INF;
+  for(int i = 1; i <= max_val; i++) {
+    int tmp = 0;
+    rep(j,N) {
+      tmp += (vec[j]-i)*(vec[j]-i);
+    }
+    ans = min(ans, tmp);
+  }
+  cout << ans << endl;
+  return 0;
+}
