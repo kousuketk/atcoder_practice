e59075f34f7ab75a63266f576f2ee46561b53dba
kousuke takahashi <pamarikomi_t@icloud.com>
2022-04-13 23:55:56 +0900

    modify contenst template

--- contests/D.cpp
+++ contests/D.cpp
@@ -10,12 +10,6 @@ using Graph = vector<vector<int>>;
 const int INF = 1001001001;
 // const ll INF = 1100100100100100100;
 const ll MOD = 1000000007;
-void printVec(vector<int> &vec) {
-  for (auto itr = vec.begin(); itr != vec.end(); itr++) cout << *itr << " "; 
-  cout << endl;
-}
-vector<int> next_h = {1, -1, 0, 0};
-vector<int> next_w = {0, 0, 1, -1};
 
 int main() {
 }
