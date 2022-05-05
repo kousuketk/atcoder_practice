823309566f9510f8e02ed7b90e9aa224a7d98e70
kousuke takahashi <pamarikomi_t@icloud.com>
2022-05-05 11:16:14 +0900

    created python template

---
+++ contentsPython/E.py
@@ -0,0 +1,18 @@
+# 文字列を受け取る場合
+S = input()
+
+# 整数を受け取る場合
+N = int(input())
+
+# 小数を受け取る場合
+F = float(input())
+
+# ------ 複数 ------
+# 文字列を受け取る場合
+A, B = input().split()
+
+# 整数を受け取る場合
+X, Y, Z = map(int, input().split())
+
+# 小数を受け取る場合
+H, M, S = map(float, input().split())
